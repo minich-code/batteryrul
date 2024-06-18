2dffe96ffcce4413cabb72fa2d04a01f06147391
Western Onzere <minichworks@gmail.com>
2024-06-18 23:26:26 +0300

    prediction pipeline

---
+++ src/RULBattery/pipelines/pip_06_prediction_pipeline.py
@@ -0,0 +1,60 @@
+import numpy as np 
+import pandas as pd 
+import os 
+import sys 
+
+from src.RULBattery import logging
+from src.RULBattery.exception import FileOperationError
+from src.RULBattery.utils.commons import load_object
+
+
+# Create a class for the prediction pipeline 
+class PredictionPipeline:
+    def __init__(self):
+        pass 
+
+    def make_predictions(self, features):
+        try:
+            logging.info("Making predictions")
+
+            # Define the model and preprocessor_obj 
+            model_path = os.path.join("artifacts", "model_trainer", "model.joblib")
+            preprocessor_path = os.path.join("artifacts", "data_transformation", "preprocessor_obj.joblib")
+            
+            # Load the preprocessor and the model 
+            preprocessor_obj = load_object(file_path=preprocessor_path)
+            model = load_object(file_path=model_path)
+
+            # Transform the features 
+            features_transformed = preprocessor_obj.transform(features)
+
+            # Make predictions 
+            predictions = model.predict(features_transformed)
+
+            # Return the predictions 
+            return predictions
+        
+        except Exception as e:
+            raise FileOperationError (e, sys)
+        
+
+# Create a class to represent input features 
+class CustomData:
+    def __init__(self, **kwargs):
+        # Initiate the attributes using kwargs
+        for key, value in kwargs.items():
+            setattr(self, key, value)
+
+    # Define a method to convert data object to a dataframe 
+    def get_data_as_dataframe(self):
+        try:
+            # log message 
+            logging.info("Converting data object to a dataframe")
+            # Convert the data object to a dataframe 
+            data_dict = {key: [getattr(self, key)] for key in vars(self)}
+
+            # Convert the dictionary to dataframe in the return  
+            return pd.DataFrame(data_dict)
+        
+        except Exception as e:
+            raise FileOperationError(e, sys)
